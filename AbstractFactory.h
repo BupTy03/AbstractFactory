32604b48116b9401b313f82cb0c2d5fd10ab8b47
Anton <anton.lagosha.99@mail.ru>
2020-10-08 18:28:49 +0500

    initial commit

---
+++ AbstractFactory.h
@@ -0,0 +1,40 @@
+#ifndef ABSTRACTFACTORY_ABSTRACTFACTORY_H
+#define ABSTRACTFACTORY_ABSTRACTFACTORY_H
+
+#include <utility>
+
+
+template<typename AbstractProduct,
+        typename IdentifierType,
+        typename ProductCreator,
+        template<typename...> typename AssociativeContainer
+        >
+class Factory
+{
+public:
+    bool Register(const IdentifierType& id, ProductCreator&& creator)
+    {
+        return associations_.emplace(id, std::forward<ProductCreator>(creator)).second;
+    }
+
+    bool Unregister(const IdentifierType& id)
+    {
+        return associations_.erase(id) == 1;
+    }
+
+    template<typename... Args>
+    AbstractProduct* Create(const IdentifierType& id, Args&&... args)
+    {
+        auto it = associations_.find(id);
+        if(it == associations_.end())
+            return nullptr;
+
+        return it->second(std::forward<Args>(args)...);;
+    }
+
+private:
+    AssociativeContainer<IdentifierType, ProductCreator> associations_;
+};
+
+
+#endif //ABSTRACTFACTORY_ABSTRACTFACTORY_H
---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include "AbstractFactory.h"
+
+#include <iostream>
+#include <typeindex>
+#include <functional>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <vector>
+
+
+class Shape {
+public:
+    explicit Shape(int arg1, const std::string& arg2) {}
+    virtual ~Shape() = default;
+    virtual void draw() const = 0;
+};
+
+class Line : public Shape {
+public:
+    explicit Line(int arg1, const std::string& arg2) : Shape(arg1, arg2) {}
+    virtual void draw() const { std::cout << "Drawing line..." << std::endl; }
+};
+
+class Triangle : public Shape {
+public:
+    explicit Triangle(int arg1, const std::string& arg2) : Shape(arg1, arg2) {}
+    virtual void draw() const { std::cout << "Drawing triangle..." << std::endl; }
+};
+
+class Circle : public Shape {
+public:
+    explicit Circle(int arg1, const std::string& arg2) : Shape(arg1, arg2) {}
+    virtual void draw() const { std::cout << "Drawing circle..." << std::endl; }
+};
+
+
+ using ShapeFactory = Factory<Shape, std::type_index, std::function<Shape*(int, const std::string&)>, std::unordered_map>;
+
+// Можем использовать следующим образом: using ShapeFactory = Factory<Shape, int, Shape*(*)(int, const std::string&), std::map>;
+// вместо std::type_index, можем использовать int - целочисленный идентификатор
+// вместо std::function<Shape*(int, const std::string&)> - можем использовать обычный указатель на функцию Shape*(*)(int, const std::string&)
+// вместо std::unordered_map - можем исползовать std::map или любой другой ассоциативный контейнер с похожим API
+
+
+struct CreateTriangleFunctor {
+    Triangle* operator()(int arg1, const std::string& arg2) const {
+        return new Triangle(arg1, arg2);
+    }
+};
+
+Shape* CreateCircleFunction(int arg1, const std::string& arg2) {
+    return new Circle(arg1, arg2);
+}
+
+
+int main()
+{
+    ShapeFactory factory;
+    factory.Register(typeid(Line), [](int arg1, const std::string& arg2) { return new Line(arg1, arg2); });
+    factory.Register(typeid(Triangle), CreateTriangleFunctor());
+    factory.Register(typeid(Circle), CreateCircleFunction);
+
+    std::vector<std::unique_ptr<Shape>> shapes;
+    shapes.reserve(3);
+    shapes.emplace_back(factory.Create(typeid(Line), 0, "Line"));
+    shapes.emplace_back(factory.Create(typeid(Triangle), 1, "Triangle"));
+    shapes.emplace_back(factory.Create(typeid(Circle), 2, "Circle"));
+
+    for(const auto& shape : shapes)
+        shape->draw();
+
+    return 0;
+}
